91740b1b7d479fc6017b612beb21941574a9c40c
Filipe Paiva <filipe.p@soltroiavillas.com>
2021-04-26 15:02:17 +0100

    provision.sh

--- provision.sh
+++ provision.sh
@@ -1,1 +1,33 @@
 #!/bin/bash
+
+if (( $# > 0 ))
+then
+  hostname=$1
+else
+  echo "WTF: you must supply a hostname or IP address" 1>&2
+  exit 1
+fi
+
+# hostname=$1
+
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/FilipePaivaKey.pem ec2-user@$hostname '
+
+
+sudo yum -y install httpd
+if rpm -qa | grep "^httpd-[0-9]" >/dev/null 2>&1
+then
+  sudo systemctl start httpd
+else
+  exit 1
+fi
+
+sudo sh -c "cat >/var/www/html/index.html <<_END_
+<h1>
+ Yellowww Server 1 🔥
+</h1>
+
+<h3>refresh for a new picture</h3>
+<img src="https://source.unsplash.com/random" alt="">
+
+_END_"
+'
